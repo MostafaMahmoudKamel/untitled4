3f05913cdbffe3650a2bce02712992813f30a1d4
MostafaMahmoudKamel <mk4274713@gmail.com>
2023-02-07 04:34:46 +0200

    ne2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    // Try adding program arguments via Run/Debug configuration.
     print("desha")
 
-    // Try adding program arguments via Run/Debug configuration.
+    println("Hello World!")
+    print("my name desha")
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
